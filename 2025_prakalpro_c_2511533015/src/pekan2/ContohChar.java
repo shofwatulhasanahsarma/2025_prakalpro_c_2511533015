0db84eb21fa67389d629f2835364b358c55eca62
FTI-LAB03-PC01 <FTI-LAB03-PC01@FTI-LAB03-PC01>
2025-09-16 11:22:36 +0700

    pratikum2_kelasc

---
+++ 2025_prakalpro_c_2511533015/src/pekan2/ContohChar.java
@@ -0,0 +1,38 @@
+// Deklarasi variabel char
+package pekan2;
+
+public class ContohChar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// Deklarasivariabel chsr
+		char huruf1 = 'A';
+		char huruf2 = 'B';
+		char angka = '7';
+		char simbol = '#';
+		
+		// Menampilkan nilai variabel char
+		System.out.println("Contoh variabel char:");
+		System.out.println("Huruf pertama:"+ huruf1);
+		System.out.println("Huruf kedua:"+ huruf2);
+		System.out.println("Angka:" + angka);
+		System.out.println("Simbol:" + simbol);
+		
+		// Operasi demgan char (berdasarkan kode Unicode/ASCII)
+		char huruf3 = (char) (huruf1 + 1); // A (65) + 1 + B (66)
+		System.out.println("Huruf1 + 1 = " + huruf3);
+		
+		// Char juga bisa disimpan dalam Integer (ASCII/Unicode value)
+		int kodeHuruf = huruf1;
+		String biner1 = String.format("%8s", Integer.toBinaryString(huruf1)).replace(' ','0');
+		System.out.println("Kode ASIIC dari " + huruf1 + "=" + kodeHuruf);
+		System.out.println("Kode BINER dari " + huruf1 + "=" + biner1);
+		
+		// Menggabungkan char menjadi string
+		String kata ="" + huruf1 + huruf2 + angka + simbol;
+		System.out.println("Gabungan char menjadi string:" + kata);
+		
+	}
+
+}
---
+++ 2025_prakalpro_c_2511533015/src/pekan2/KelilingLingkaran.java
@@ -0,0 +1,15 @@
+package pekan2;
+
+public class KelilingLingkaran {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+			final double PI = 3.14;	/* Definisi Konstanta */
+			double radius = 30;		/* Deklarasi variabel */
+				System.out.println("Keliling = " + 2*PI*radius);
+
+
+	}
+
+}
