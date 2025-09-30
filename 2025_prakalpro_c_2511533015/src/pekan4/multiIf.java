0518ec980784c82503128f29af18243f437f3f40
FTI-LAB03-PC01 <FTI-LAB03-PC01@FTI-LAB03-PC01>
2025-09-30 11:24:46 +0700

    Merge branch 'master' of https://github.com/shofwatulhasanahsarma/2025_prakalpro_c_2511533015

---
+++ 2025_prakalpro_c_2511533015/src/pekan4/multiIf.java
@@ -0,0 +1,30 @@
+package pekan4;
+import java.util.Scanner;
+public class multiIf {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int umur;
+		char sim;
+		Scanner a= new Scanner(System.in);
+		System.out.print("Input umur anda: ");
+		umur= a.nextInt();
+		System.out.print("Apakah Anda Sudah Punya Sim C: ");
+		sim=a.next().charAt(0);
+		a.close();
+		if((umur >= 17)&&(sim=='y')) {
+			System.out.println("Anda Sudah Dewasa dan boleh bawa motor");
+		}
+		if((umur >= 17)&&(sim!='y')) {
+			System.out.println("Anda Sudah Dewasa tetapi tidak boleh bawa motor");
+		}
+		if((umur < 17)&&(sim!='y')) {
+			System.out.println("Anda Belum Cukup Umur bawa motor");
+		}
+		if((umur <17)&&(sim=='y')) {
+			System.out.println("Anda Belum Cukup Umur punya SIM");
+		}
+
+	}
+
+}
